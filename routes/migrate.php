e27997e939e407520255c3b7aba3020784d3b8bd
higghxmaath <ayanmattex@gmail.com>
2026-01-26 14:07:10 +0100

    migration route

---
+++ routes/migrate.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/__migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return Artisan::output();
+});
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+require __DIR__.'/migrate.php';
+
 
 Route::get('/', function () {
     return view('welcome');
