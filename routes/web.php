dc51549457bb170d8b737efd33b4e763dbbf3513
higghxmaath <ayanmattex@gmail.com>
2026-01-29 17:48:12 +0100

    Railway deploy

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,8 @@ require __DIR__.'/migrate.php';
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/__migrate', function () {
+    \Artisan::call('migrate', ['--force' => true]);
+    return nl2br(\Artisan::output());
+});
